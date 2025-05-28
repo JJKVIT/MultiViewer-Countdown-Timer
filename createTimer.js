34b08d000bdd5570259035c22b3ebccd462c50f2
Joseph JK VIT <115476354+JJKVIT@users.noreply.github.com>
2025-05-28 20:54:49 +0530

    Live -> Watch Replay Update fix (couldnt test)

--- createTimer.js
+++ createTimer.js
@@ -1,227 +1,257 @@
-let lastProcessedUrl = null;
-let weekendInterval = null; 
-let count = 0; 
-let currentTab = null;
-let newDiv = null;
-let weekend = null;
-let cardHeader = null;
-let timerInterval = null;
-
-const urlChangeObserver = new MutationObserver(()=>{
-    if (lastProcessedUrl !== location.href) {
-        tabsChangeObserver.disconnect();
-        weekendObserver.disconnect();
-        console.log("URL changed!");
-        if (weekendInterval != null) {
-            clearInterval(weekendInterval);
-            weekendInterval = null;
-            count = 0; 
-        }
-        if(cardHeader!=null && newDiv!=null){
-            cardHeader.removeChild(newDiv);
-            newDiv = null;
-        }
-        if(timerInterval!=null){
-            clearInterval(timerInterval);
-            timerInterval = null;
-        }
-        lastProcessedUrl = location.href; 
-        findWeekend();
-    }
-});
-
-urlChangeObserver.observe(document, { subtree: true, childList: true });
-
-const tabsChangeObserver = new MutationObserver(()=>{
-    let tabTitle =  (document.querySelector('.MuiButtonBase-root.MuiTab-root.MuiTab-textColorPrimary.Mui-selected.css-x0hk9u')).textContent;
-    if(currentTab != tabTitle && currentTab!=null){
-        if(timerInterval!=null){
-            clearInterval(timerInterval);
-            timerInterval = null;
-        }
-        weekendObserver.disconnect();
-        console.log("Tabs changed!");
-        innerElement(weekend);
-        currentTab = tabTitle;
-    }
-});
-
-const weekendObserver = new MutationObserver(()=>{
-    console.log("Weekend element changed!");
-    innerElement(weekend);
-});
-
-function findWeekend() {
-    if (weekendInterval === null) {
-        count = 0;
-        weekendInterval = setInterval(()=>{
-            console.log("Checking for weekend element... Attempt:", count); 
-            weekend = document.querySelector('.MuiPaper-root.MuiPaper-elevation.MuiPaper-rounded.MuiPaper-elevation1.MuiCard-root.css-1wou66g');
-            if(weekend!=null || count>=5){ 
-                console.log("Weekend element found!");
+document.addEventListener('DOMContentLoaded',()=>{
+    let lastProcessedUrl = null;
+    let weekendInterval = null; 
+    let count = 0; 
+    let currentTab = null;
+    let newDiv = null;
+    let weekend = null;
+    let cardHeader = null;
+    let timerInterval = null;
+    
+    const urlChangeObserver = new MutationObserver(()=>{
+        if (lastProcessedUrl !== location.href) {
+            tabsChangeObserver.disconnect();
+            weekendObserver.disconnect();
+            console.log("URL changed!");
+            if (weekendInterval != null) {
                 clearInterval(weekendInterval);
-                weekendInterval = null; 
-                if (weekend != null) {
-                    let tabs = document.querySelector('.MuiTabs-flexContainer.css-k008qs');
-                    
-                    currentTab = (document.querySelector('.MuiButtonBase-root.MuiTab-root.MuiTab-textColorPrimary.Mui-selected.css-x0hk9u')).textContent;
-                    tabsChangeObserver.observe(tabs, { subtree: true, childList: true });
-                    
-                    createElement(weekend);
-                }
+                weekendInterval = null;
+                count = 0; 
             }
-            count++;
-        }, 2000);
-    }
-}
-
-async function createElement(weekend) {
-    cardHeader = weekend.querySelector('.MuiCardHeader-content');
-
-    if (cardHeader && !cardHeader.querySelector('.timerDiv')) { 
-        cardHeader.style.display = 'flex';
-        cardHeader.style.alignItems = 'center';
-
-        newDiv = document.createElement('div');
-        newDiv.classList.add('timerDiv'); 
-        
-        await innerElement(weekend);
-        
-        cardHeader.appendChild(newDiv);
-        console.log("Custom element created and appended.");
-    }
-}
-
-async function innerElement(weekend){
-    console.log("updating element");
-    let weekendInfo = await getLatest(weekend);    
-
-    let innerDiv = document.querySelector('.timer-div');
-    if(innerDiv==null){
-        innerDiv = document.createElement('div');
-        innerDiv.classList.add('timer-div');
-
-        if (newDiv && !newDiv.contains(innerDiv)) {
-             newDiv.appendChild(innerDiv);
-        }
-    }
-
-    if(timerInterval!=null){
-        clearInterval(timerInterval);
-        timerInterval = null;
-    }
-
-    newDiv.classList.remove("Live");
-
-    if(weekendInfo.status == "WATCH LIVE"){
-        console.log("Live");
-
-        newDiv.classList.add("Live");
-
-        innerDiv.innerHTML = `<a href="${weekendInfo.link} " ${weekendInfo.category != "F1" || weekendInfo.eventName.contains(weekendInfo.category)?'target="_blank"':""} style="color: rgb(255,255,255); text-decoration: none; cursor: pointer">Live: ${weekendInfo.category} ${weekendInfo.eventName}</a>`;
-
-        console.log(weekendInfo.div);
-        let l = weekendObserver.observe(weekendInfo.div, { subtree: true, childList: true });
-        console.log("l: ",l);
-        
-        var anchor = innerDiv.querySelector('a');
-        if(anchor) {
-            anchor.addEventListener('click', function(event) {
-                event.stopPropagation();
-            });
-        }
-
-        innerDiv.onclick = function(e) {
-            e.stopPropagation();
-            var anchor = innerDiv.querySelector('a');
-            if(anchor) {
-                anchor.click();
+            if(cardHeader!=null && newDiv!=null){
+                cardHeader.removeChild(newDiv);
+                newDiv = null;
             }
-        };
-    }
-    else if(weekendInfo.status == "UPCOMING"){
-
-        const timeObj = convertToDateTime(weekendInfo.timeStr);
-
-        if (timeObj === null || isNaN(timeObj.getTime())) {
-             innerDiv.innerText = weekendInfo.category +" "+ weekendInfo.eventName + ": Invalid Time";
-             console.error("Failed to parse upcoming event time:", weekendInfo.timeStr);
-             return; 
-        }
-
-        let timeLeftMillis = timeObj.getTime() - Date.now(); 
-
-        const updateCountdown = () => {
-            if (timeLeftMillis <= 0) {
-                innerDiv.innerText = weekendInfo.category +" "+ weekendInfo.eventName + ": Live!";
+            if(timerInterval!=null){
                 clearInterval(timerInterval);
                 timerInterval = null;
-                innerElement(weekend);
-            } else {
-                innerDiv.innerText = weekendInfo.category +" "+ weekendInfo.eventName + ": " + formatCountdown(timeLeftMillis);
-                timeLeftMillis -= 1000;
             }
-        };
-        timerInterval = setInterval(updateCountdown, 1000); 
+            lastProcessedUrl = location.href; 
+            findWeekend();
+        }
+    });
+    
+    urlChangeObserver.observe(document, { subtree: true, childList: true });
+    
+    const tabsChangeObserver = new MutationObserver(()=>{
+        let tabTitle =  (document.querySelector('.MuiButtonBase-root.MuiTab-root.MuiTab-textColorPrimary.Mui-selected.css-x0hk9u')).textContent;
+        if(currentTab != tabTitle && currentTab!=null){
+            if(timerInterval!=null){
+                clearInterval(timerInterval);
+                timerInterval = null;
+            }
+            weekendObserver.disconnect();
+            console.log("Tabs changed!");
+            innerElement(weekend);
+            currentTab = tabTitle;
+        }
+    });
+    
+    const weekendObserver = new MutationObserver((statusDiv)=>{
+        console.log("Weekend element changed!");
+        if(statusDiv.innerText == "WATCH REPLAY" ){
+            console.log("time left null");
+            innerElement(weekend);
+        }
+    });
+    
+    function findWeekend() {
+        if (weekendInterval === null) {
+            count = 0;
+            weekendInterval = setInterval(()=>{
+                console.log("Checking for weekend element... Attempt:", count); 
+                weekend = document.querySelector('.MuiPaper-root.MuiPaper-elevation.MuiPaper-rounded.MuiPaper-elevation1.MuiCard-root.css-1wou66g');
+                if(weekend!=null || count>=5){ 
+                    console.log("Weekend element found!");
+                    clearInterval(weekendInterval);
+                    weekendInterval = null; 
+                    if (weekend != null) {
+                        // Cache this if it doesn't change frequently within this scope
+                        let tabs = document.querySelector('.MuiTabs-flexContainer.css-k008qs'); 
+                        let selectedTab = document.querySelector('.MuiButtonBase-root.MuiTab-root.MuiTab-textColorPrimary.Mui-selected.css-x0hk9u');
+                        
+                        if (tabs && selectedTab) {
+                            currentTab = selectedTab.textContent;
+                            tabsChangeObserver.observe(tabs, { subtree: true, childList: true });
+                            createElement(weekend);
+                        }
+                    }
+                }
+                count++;
+            }, 2000);
+        }
     }
-    else{
-        innerDiv.innerText = weekendInfo.status;
-    }   
-}
-
-async function getLatest(){
-    let statusDiv = weekend.querySelector('.MuiList-root');
-
-    if(statusDiv!=null){
-        for(let i = 0; i<statusDiv.children.length; i++){
-            let status = {
-                "status" : statusDiv.children[i].children[3].innerText,
-                "category" : statusDiv.children[i].children[0].innerText
+    
+    async function createElement(weekend) {
+        cardHeader = weekend.querySelector('.MuiCardHeader-content');
+    
+        if (cardHeader && !cardHeader.querySelector('.timerDiv')) { 
+            cardHeader.style.display = 'flex';
+            cardHeader.style.alignItems = 'center';
+    
+            newDiv = document.createElement('div');
+            newDiv.classList.add('timerDiv'); 
+            
+            await innerElement(weekend);
+            
+            cardHeader.appendChild(newDiv);
+            console.log("Custom element created and appended.");
+        }
+    }
+    
+    async function innerElement(weekend){
+        console.log("updating element");
+        let weekendInfo = await getLatest(weekend);    
+    
+        let innerDiv = newDiv ? newDiv.querySelector('.timer-div') : null; 
+        if(!innerDiv){
+            innerDiv = document.createElement('div');
+            innerDiv.classList.add('timer-div');
+    
+            if (newDiv) {
+                 newDiv.appendChild(innerDiv);
             }
-            console.log(status.status);
-            if(status.status == "UPCOMING"){
-                status.timeStr = statusDiv.children[i].children[2].innerText;
-                status.eventName = statusDiv.children[i].children[1].innerText;
+        }
+    
+        if(timerInterval!=null){
+            clearInterval(timerInterval);
+            timerInterval = null;
+        }
+    
+        newDiv.classList.remove("Live");
+    
+        if(weekendInfo.status == "WATCH LIVE"){
+            console.log("Live");
+    
+            newDiv.classList.add("Live");
+    
+            innerDiv.innerHTML = `<a href="${weekendInfo.link} " ${weekendInfo.category != "F1" || weekendInfo.eventName.contains(weekendInfo.category)?'target="_blank"':""} style="color: rgb(255,255,255); text-decoration: none; cursor: pointer">Live: ${weekendInfo.category} ${weekendInfo.eventName}</a>`;
+    
+            setTimeout(()=>{
+                console.log(weekendInfo.statusDiv);
+                let l = weekendObserver.observe(weekendInfo.statusDiv, { subtree: true, childList: true }, weekendInfo.statusDiv);
+                console.log("l: ",l);
 
-                return status;
-            }
-            else if(status.status == "WATCH LIVE"){
-                status.eventName = statusDiv.children[i].children[1].innerText;
-                status.link =  statusDiv.children[i].children[3].querySelector('a').href;
-                status.div = statusDiv.children[i].children[3];
-                
-                if(!status.eventName.contains("Pre-")){
-                    return status;
+                if(weekendInfo.statusDiv.innerText == "WATCH REPLAY" ){
+                    weekendObserver.disconnect();
+                    console.log("time left null");
+                    innerElement(weekend);
                 }
-                
-                let nextElement = statusDiv.children[i+1]
-                if(nextElement!=null && nextElement.children[3].innerText == "WATCH LIVE"){
-                    status.eventName = nextElement.children[1].innerText;
-                    status.link =  nextElement.children[3].querySelector('a').href;
-                    status.div = nextElement.children[3];
-                    
-                    return status;
-                }
 
-                return status;
+            }, weekendInfo.timeLeft);
+            
+            var anchor = innerDiv.querySelector('a');
+            if(anchor) {
+                anchor.addEventListener('click', function(event) {
+                    event.stopPropagation();
+                });
             }
+    
+            innerDiv.onclick = function(e) {
+                e.stopPropagation();
+                var anchor = innerDiv.querySelector('a');
+                if(anchor) {
+                    anchor.click();
+                }
+            };
             
         }
-        return {
-            "status" : "Completed"
-        };
+        else if(weekendInfo.status == "UPCOMING"){
+    
+            const timeObj = convertToDateTime(weekendInfo.timeStr);
+    
+            if (timeObj === null || isNaN(timeObj.getTime())) {
+                 innerDiv.innerText = weekendInfo.category +" "+ weekendInfo.eventName + ": Invalid Time";
+                 console.error("Failed to parse upcoming event time:", weekendInfo.timeStr);
+                 return; 
+            }
+    
+            let timeLeftMillis = timeObj.getTime() - Date.now(); 
+    
+            const updateCountdown = () => {
+                if (timeLeftMillis <= 0) {
+                    innerDiv.innerText = weekendInfo.category +" "+ weekendInfo.eventName + ": Live!";
+                    clearInterval(timerInterval);
+                    timerInterval = null;
+                    innerElement(weekend);
+                } else {
+                    innerDiv.innerText = weekendInfo.category +" "+ weekendInfo.eventName + ": " + formatCountdown(timeLeftMillis);
+                    timeLeftMillis -= 1000;
+                }
+            };
+            timerInterval = setInterval(updateCountdown, 1000); 
+        }
+        else{
+            innerDiv.innerText = weekendInfo.status;
+        }   
     }
-    return {
-        "status" : "Error Loading Data"
-    };
-}
+    
+    async function getLatest(){
+        if (!weekend) return { "status": "Error: Weekend element not found" };
+        const statusDiv = weekend.querySelector('.MuiList-root'); 
+    
+        if(statusDiv && statusDiv.children.length > 0){
+            for(let i = 0; i < statusDiv.children.length; i++){
+                const child = statusDiv.children[i];
+                
+                if (!child || child.children.length < 4) {return { "status": "Error: Couldnt Find child element" }};
+    
+                const categoryText = child.children[0].innerText;
+                const eventNameText = child.children[1].innerText;
+                const statusText = child.children[3].innerText;
+                const childStatusDiv = child.children[3];
+    
+                const status = {
+                    "status": statusText,
+                    "category": categoryText,
+                    "eventName": eventNameText,
+                    "statusDiv": childStatusDiv
+                };
+                
+                console.log(status.status);
+                if(status.status === "UPCOMING"){
+                    status.timeStr = child.children[2].innerText;
+                    return status;
+                }
+                else if(status.status === "WATCH LIVE"){
+                    const anchor = child.children[3].querySelector('a');
+                    status.link = anchor?anchor.href:null;
+                    
+                    if(!status.eventName.includes("Pre-")){
+                        status.timeLeft = getTimeRemaining(child.children[2].innerText);
+                        return status;
+                    }
+                    
+                    const nextElement = statusDiv.children[i+1];
+                    if(nextElement && nextElement.children.length > 3 && nextElement.children[3].innerText === "WATCH LIVE"){
+                        status.eventName = nextElement.children[1].innerText;
+                        const nextAnchor = nextElement.children[3].querySelector('a');
+                        status.link = nextAnchor?nextAnchor.href:null;
+                        status.timeLeft = getTimeRemaining(nextElement.children[2].innerText);
+                        
+                        return status;
+                    }
+                    
+                    status.timeLeft = getTimeRemaining(child.children[2].innerText);
+                    return status;
+                }
+            }
+            return { "status": "Completed" };
+        }
+        return { "status": "Error Loading Data" };
+    }
+})
 
 function convertToDateTime(timeStr){
-    const part = (timeStr.split('–')[0]).replaceAll(",","");
-    const currentYear = new Date().getFullYear();
-    const datetime = new Date(part + " " +currentYear);
-    
-    return datetime;
+    try {
+        const part = (timeStr.split('–')[0]).replace(/,/g, "").trim();
+        const currentYear = new Date().getFullYear();
+        const datetime = new Date(part + " " + currentYear);
+        return isNaN(datetime.getTime()) ? null : datetime;
+    } catch (error) {
+        console.error('Error parsing date:', timeStr, error);
+        return null;
+    }
 }
 function formatCountdown(ms) {
     const seconds = Math.floor((ms / 1000) % 60);
@@ -235,3 +265,14 @@ function formatCountdown(ms) {
     }
     return `${pad(days)}d ${pad(hours)}h ${pad(minutes)}m ${pad(seconds)}s`;
 }
+
+function getTimeRemaining(timeStr){
+    const endStr = (timeStr.split('–')[1]).replace(/,/g, "");
+    const date = (((new Date(endStr)).setDate(Date.now().getDate())).setMonth(Date.now().getMonth())).setFullYear(Date.now().getFullYear());
+
+    return (date.getTime() - (Date.now()).getTime());
+}
+
+/*
+MuiCircularProgress-root MuiCircularProgress-indeterminate MuiCircularProgress-colorPrimary css-n9w9ys => loading bar maybe check if this is no longer in the doc instead
+*/
